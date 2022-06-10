5052583e24f4c50e2ae8e4b0567ac756262426f9
LauraRLT <LauraRLTravis@gmail.com>
2022-06-10 17:46:04 -0500

    add onFocus to input to select all text

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,17 +7,22 @@ export default class Playlist extends React.Component {
     super(props)
 
     this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleFocus = this.handleFocus.bind(this)
   }
 
   handleNameChange(e) {
     this.props.onNameChange(e.target.value)
   }
 
+  handleFocus(e) {
+    e.target.select();
+  }
+
   render() {  
     return (
         <div className="Playlist">
           <p className='instructions'>Click Below to Change Playlist Name</p>
-            <input value={this.props.playlistName} onChange={this.handleNameChange}/>
+            <input value={this.props.playlistName} onChange={this.handleNameChange} onFocus={this.handleFocus}/>
                 <TrackList 
                   tracks={this.props.playlistTracks} 
                   onRemove={this.props.onRemove}
