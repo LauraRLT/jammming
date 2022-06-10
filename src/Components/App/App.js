ed7e6b5292a4f3d55c91f8984220d7d439e69db3
LauraRLT <LauraRLTravis@gmail.com>
2022-06-10 16:35:56 -0500

    get accesstoken immediately

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -72,6 +72,7 @@ export default class App extends React.Component {
   }
 
   render() {
+    Spotify.getAccessToken()
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
